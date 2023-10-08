75b01c305f9838e1850c47a70582f7c9537e287b
A1034512 <lucasrodrigues@educarbrasil.org.br>
2023-10-08 13:54:39 -0300

    Template Method

--- BehaviourPattern/TemplateMethod/View/index.php
+++ BehaviourPattern/TemplateMethod/View/index.php
@@ -2,3 +2,34 @@
 
 namespace Lucas\TemplateMethod;
 
+function simulateNetworkLatency()
+{
+    $i = 0;
+    while ($i < 5) {
+        echo ".";
+        sleep(1);
+        $i++;
+    }
+}
+
+echo "Username: \n";
+$username = readline();
+echo "Password: \n";
+$password = readline();
+echo "Message: \n";
+$message = readline();
+
+echo "\nChoose the social network to post the message:\n" .
+    "1 - Facebook\n" .
+    "2 - Twitter\n";
+$choice = readline();
+
+// Now, let's create a proper social network object and send the message.
+if ($choice == 1) {
+    $network = new Facebook($username, $password);
+} elseif ($choice == 2) {
+    $network = new Twitter($username, $password);
+} else {
+    die("Sorry, I'm not sure what you mean by that.\n");
+}
+$network->post($message);
