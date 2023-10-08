0b2cbc55238cb450a62402548740c0633af5df9a
A1034512 <lucasrodrigues@educarbrasil.org.br>
2023-10-08 10:54:49 -0300

    Composite

---
+++ StructuralPattern/Composite/Service/FieldSet.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Lucas\Composite;
+
+class Fieldset extends FieldComposite
+{
+    public function render(): string
+    {
+        // Note how the combined rendering result of children is incorporated
+        // into the fieldset tag.
+        $output = parent::render();
+        
+        return "<fieldset><legend>{$this->title}</legend>\n$output</fieldset>\n";
+    }
+}
