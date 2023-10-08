143b74e7d8f6cfb64f4ee3408e325f171f653ebc
A1034512 <lucasrodrigues@educarbrasil.org.br>
2023-10-08 13:35:51 -0300

    Observer pattern

--- BehaviourPattern/Observer/View/index.php
+++ BehaviourPattern/Observer/View/index.php
@@ -2,3 +2,19 @@
 
 namespace Lucas\Observer;
 
+$repository = new UserRepository();
+$repository->attach(new Logger(__DIR__ . "/log.txt"), "*");
+$repository->attach(new OnboardingNotification("1@example.com"), "users:created");
+
+$repository->initialize(__DIR__ . "/users.csv");
+
+// ...
+
+$user = $repository->createUser([
+    "name" => "John Smith",
+    "email" => "john99@example.com",
+]);
+
+// ...
+
+$repository->deleteUser($user);
